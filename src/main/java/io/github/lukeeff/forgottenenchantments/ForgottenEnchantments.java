f9e14cb22c69b0bbe647ff0b5d4dedc15b15b006
lukeeff <luke4nandez@gmail.com>
2020-04-21 14:04:30 -0700

    Initial commit

---
+++ src/main/java/io/github/lukeeff/forgottenenchantments/ForgottenEnchantments.java
@@ -0,0 +1,17 @@
+package io.github.lukeeff.forgottenenchantments;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ForgottenEnchantments extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
